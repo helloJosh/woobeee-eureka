1b33e80cee7f5165dcadbc474db4a5574386dbcd
helloJosh <kimjoshua135@gmail.com>
2024-10-15 15:23:17 +0900

    feat:security updated

--- src/main/java/com/woobeee/eureka/config/SecurityConfig.java
+++ src/main/java/com/woobeee/eureka/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity(debug = false)
 @Configuration
@@ -17,11 +16,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable);
         http.authorizeHttpRequests(authorizeRequests ->
-                authorizeRequests
-                        .requestMatchers(new AntPathRequestMatcher("/eureka/**", "/"))
-                        .authenticated()
-                        .anyRequest()
-                        .permitAll()
+                authorizeRequests.anyRequest().authenticated()
         );
         http.httpBasic(Customizer.withDefaults());
         return http.build();
