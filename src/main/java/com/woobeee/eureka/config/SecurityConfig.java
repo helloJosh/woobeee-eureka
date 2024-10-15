2679a81ff206a5b7ad10e56f45a171a854f5f888
helloJosh <kimjoshua135@gmail.com>
2024-10-15 15:18:55 +0900

    feat: eureka security added

--- src/main/java/com/woobeee/eureka/config/SecurityConfig.java
+++ src/main/java/com/woobeee/eureka/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity(debug = false)
 @Configuration
@@ -16,9 +17,12 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable);
         http.authorizeHttpRequests(authorizeRequests ->
-                authorizeRequests.anyRequest().authenticated()
+                authorizeRequests
+                        .requestMatchers(new AntPathRequestMatcher("/eureka/**", "/"))
+                        .authenticated()
+                        .anyRequest()
+                        .permitAll()
         );
-
         http.httpBasic(Customizer.withDefaults());
         return http.build();
     }
