765db6d3ae610b2f86198b12f101dda30ca25dcc
helloJosh <kimjoshua135@gmail.com>
2024-10-15 15:09:06 +0900

    first commit

---
+++ src/test/java/com/woobeee/eureka/BlogEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woobeee.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
